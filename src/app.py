eab2e21f8b96bfebdf4fd77c6b628a3ffc4e5d14
bestiven <bbaqueroalonso@gmail.com>
2024-05-27 20:45:31 -0500

    anexo parte de cristancho

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template , url_for, redirect,request
+from flask import Flask, render_template , url_for, redirect,request, send_from_directory
 from config import *
 from models.persona import Persona 
+import os 
+from werkzeug.utils import secure_filename
 
 
 con_bd =conexion() 
@@ -41,6 +43,36 @@ def registro():
 def login():  
     return render_template('/auth/login.html')
 
+
+ALLOWED_EXTENSIONS = set(["doc","txt","docx","pdf"])
+
+app.config["UPLOAD_FOLDER"]= "src/static/uploads"
+
+def allowed_file(file):
+    file = file.split('.')
+    print(file)
+    if file[1] in ALLOWED_EXTENSIONS:
+        return True
+    return False
+
+@app.route('/proyectos', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['documento']
+        print(file, file.filename)
+        filename = secure_filename(file.filename)
+        print(filename)
+        if file and allowed_file(filename):
+            print("permitido")
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'GUARDADO EXITOSAMENTE'
+    else:
+        files = os.listdir(app.config['UPLOAD_FOLDER'])
+        return render_template('/auth/proyectos.html', files=files) 
+
+@app.route('/proyectos')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__=='__main__':
